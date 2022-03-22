81858b92d590bd5bdc301fa1800867433a30a2a2
Dustin Savage <Dnsavage@crimson.ua.edu>
2022-03-22 08:19:10 -0500

    Updated crud processing to cloud

--- Models/SaveBook.cs
+++ Models/SaveBook.cs
@@ -1,5 +1,6 @@
 using System.Data.SQLite;
 using API.Models.Interfaces;
+using MySql.Data.MySqlClient;
 
 namespace API.Models
 {
@@ -7,17 +8,29 @@ namespace API.Models
     {
         public void InsertBook(Book value)
         {
-            string cs = @"URI=file:C:\Users\Plati\Desktop\Spring2022\MIS321\repos\BookDatabase\book.db";
-            using var con = new SQLiteConnection(cs);
-            con.Open();
+            //string cs = @"URI=file:C:\Users\Plati\Desktop\Spring2022\MIS321\repos\BookDatabase\book.db";
+            //using var con = new SQLiteConnection(cs);
+            //con.Open();
 
-            string stm = @"INSERT INTO books(title, author) VALUES(@title, @author)";
+            //string stm = @"INSERT INTO books(title, author) VALUES(@title, @author)";
 
-            using var cmd = new SQLiteCommand(stm, con);
-            cmd.Parameters.AddWithValue("@title",value.Title);
-            cmd.Parameters.AddWithValue("@author",value.Author);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            DBConnect db = new DBConnect();
+            bool isOpen = db.OpenConnection();
+
+            if(isOpen){
+                MySqlConnection conn = db.GetConn();
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conn;
+
+                cmd.CommandText = @"INSERT INTO books(title, author) VALUES(@title, @author)";
+                cmd.Parameters.AddWithValue("@title",value.Title);
+                cmd.Parameters.AddWithValue("@author",value.Author);
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                db.CloseConnection();
+            }
         }
     }
 }
